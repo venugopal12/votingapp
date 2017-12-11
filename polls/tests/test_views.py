8cc0ae8692239290b167327849f4580ffa655a81
Ronnie Smith <halfpint1170@gmail.com>
2017-12-10 18:15:46 -0800

    Home page is now using a template

---
+++ polls/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+
+    #  Test that resolve('/') is a views.HomePage
+    #  Currently this doesnt work and im not sure why
+
+    def test_home_page_using_base_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'base.html')
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
 from django.views.generic import View
+from django.shortcuts import render
 
 
 class HomeView(View):
 
-    def get(self, response):
-        return HttpResponse('Hello world')
+    def get(self, request):
+        return render(request, 'base.html')
