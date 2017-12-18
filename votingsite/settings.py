d3435d844b87f2a97d22560441a6a448c292e9d9
Ronnie Smith <halfpint1170@gmail.com>
2017-12-18 12:43:12 -0800

    Disabled database for now

--- votingsite/settings.py
+++ votingsite/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'votingsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
