9ac83248bdfc89b601a2123e73fa3e4838155ba5
Ronnie Smith <halfpint1170@gmail.com>
2017-12-26 16:02:43 -0800

    Can now vote and view the results

--- polls/tests/test_views.py
+++ polls/tests/test_views.py
@@ -55,15 +55,31 @@ class ViewPollGetTest(TestCase):
         self.assertContains(response, choice_0)
         self.assertContains(response, choice_1)
 
-    def test_choices_have_css_name_of_text(self):
-        choice_0 = 'Yes of course'
-        choice_1 = 'Not really, I am full'
+    def test_choices_have_css_name_of_choice_id(self):
+        # create another poll and some choices, so our real poll isnt the first
+        random_poll = Poll.objects.create(text='Random poll')
+        for _ in range(5):
+            Choice.objects.create(text='random choice', poll=random_poll)
+
         poll = Poll.objects.create(text='Would you like a cookie?')
-        Choice.objects.create(text=choice_0, poll=poll)
-        Choice.objects.create(text=choice_1, poll=poll)
+        choice_0 = Choice.objects.create(text='Yes of course', poll=poll)
+        choice_1 = Choice.objects.create(text='Not really', poll=poll)
         response = self.client.get(f'/poll/{poll.uid}')
-        self.assertContains(response, 'name="yes-of-course"')
-        self.assertContains(response, 'name="not-really-i-am-full"')
+        self.assertContains(
+            response, 
+            f'name="choice_id" value="{choice_0.id}"'
+        )
+        self.assertContains(
+            response, 
+            f'name="choice_id" value="{choice_1.id}"'
+        )
+
+    def test_contains_hidden_field_for_poll_uid(self):
+        poll = Poll.objects.create(text='Would you like a cookie?')
+        Choice.objects.create(text='Yes of course', poll=poll)
+        Choice.objects.create(text='Not really', poll=poll)
+        response = self.client.get(f'/poll/{poll.uid}')
+        self.assertContains(response, f'name="poll_uid" value="{poll.uid}"')
 
     def test_passes_in_correct_poll(self):
         Poll.objects.create(text='some other poll')
@@ -95,3 +111,53 @@ class ViewPollPostTest(TestCase):
         })
         new_poll = Poll.objects.first()
         self.assertRedirects(response, f'/poll/{new_poll.uid}')
+
+
+class ResultsTest(TestCase):
+
+    def test_uses_results_template(self):
+        poll = Poll.objects.create(text='A')
+        response = self.client.get(f'/poll/{poll.uid}/results')
+        self.assertTemplateUsed(response, 'results.html')
+
+    def test_passes_in_correct_poll(self):
+        Poll.objects.create(text='some other poll')
+        poll = Poll.objects.create(text='The question we are asking')
+        response = self.client.get(f'/poll/{poll.uid}/results')
+        self.assertEqual(response.context['poll'], poll)
+
+
+class VoteTest(TestCase):
+
+    def test_redirects_to_results_page(self):
+        poll = Poll.objects.create(text='A')
+        choice = Choice.objects.create(text='123', poll=poll)
+        response = self.client.post(f'/vote', data={
+            'poll_uid': poll.uid,
+            'choice_id': choice.id
+        })
+        self.assertRedirects(response, f'/poll/{poll.uid}/results')
+
+    def test_increases_vote_by_one(self):
+        poll = Poll.objects.create(text='A')
+        choice = Choice.objects.create(text='123', poll=poll)
+        self.client.post(f'/vote', data={
+            'poll_uid': poll.uid,
+            'choice_id': choice.id
+        })
+        # we have to get the new choice from the database
+        # the existing one we have above wont be updated
+        self.assertEqual(Choice.objects.get(id=choice.id).votes, 1)
+
+    def test_can_vote_more_than_once(self):
+        poll = Poll.objects.create(text='A')
+        choice = Choice.objects.create(text='123', poll=poll)
+        self.client.post(f'/vote', data={
+            'poll_uid': poll.uid,
+            'choice_id': choice.id
+        })
+        self.client.post(f'/vote', data={
+            'poll_uid': poll.uid,
+            'choice_id': choice.id
+        })
+        self.assertEqual(Choice.objects.get(id=choice.id).votes, 2)
