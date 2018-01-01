54df2b32b616f76df75e911e920259967eeb6055
Ronnie Smith <halfpint1170@gmail.com>
2018-01-01 01:24:35 -0800

    Moved the local database into the root project

--- votingsite/settings.py
+++ votingsite/settings.py
@@ -133,6 +133,6 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
