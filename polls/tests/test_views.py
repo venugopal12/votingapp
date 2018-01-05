22e29d6e6b1be3280cc675c92624ded98511cb1b
Ronnie Smith <halfpint1170@gmail.com>
2018-01-05 13:35:56 -0800

    Fixed issues with removing auth that i missed

--- polls/tests/test_views.py
+++ polls/tests/test_views.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
 from polls.models import Poll, Choice
-User = get_user_model()
 
 
 class HomeTest(TestCase):
@@ -10,18 +8,10 @@ class HomeTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_uses_dashboard_template_when_logged_in(self):
-        user = User.objects.create(email='a@b.com')
-        self.client.force_login(user)
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'dashboard.html')
-
 
 class NewPollTest(TestCase):
 
     def test_new_poll_uses_new_poll_template(self):
-        user = User.objects.create(email='mary@example.com')
-        self.client.force_login(user)
         response = self.client.get('/new')
         self.assertTemplateUsed(response, 'new_poll.html')
 
