92c5111f2141f35a84c556d79026d417eccbcb69
Ronnie Smith <halfpint1170@gmail.com>
2018-01-05 18:37:22 -0800

    Simplified pages and urls, makes more sense this way

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,5 @@ from polls import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('poll/<uid>', views.PollView.as_view(), name='poll'),
-    path('poll', views.PollView.as_view(), name='poll'),
     path('poll/<uid>/results', views.ResultsView.as_view(), name='results'),
-    path('vote', views.VoteView.as_view(), name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -8,13 +8,6 @@ class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
 
-
-class PollView(View):
-
-    def get(self, request, uid):
-        poll = Poll.objects.get(uid=uid)
-        return render(request, 'poll.html', {'poll': poll})
-
     def post(self, request):
         text = request.POST['text']
         poll = Poll.objects.create(text=text)
@@ -25,19 +18,22 @@ class PollView(View):
         return redirect(f'/poll/{poll.uid}')
 
 
+class PollView(View):
+
+    def get(self, request, uid):
+        poll = Poll.objects.get(uid=uid)
+        return render(request, 'poll.html', {'poll': poll})
+
+    def post(self, request, uid):
+        poll = Poll.objects.get(uid=uid)
+        choice = Choice.objects.get(id=request.POST['choice_id'])
+        choice.vote()
+
+        return redirect(f'/poll/{poll.uid}/results')
+
+
 class ResultsView(View):
 
     def get(self, request, uid):
         poll = Poll.objects.get(uid=uid)
         return render(request, 'results.html', {'poll': poll})
-
-
-class VoteView(View):
-
-    def post(self, request):
-        poll = Poll.objects.get(uid=request.POST['poll_uid'])
-
-        choice = Choice.objects.get(id=request.POST['choice_id'])
-        choice.vote()
-
-        return redirect(f'/poll/{poll.uid}/results')
