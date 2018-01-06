da42168690403060a7a40df40cbf4ee9333fa9b8
Ronnie Smith <halfpint1170@gmail.com>
2018-01-06 15:21:04 -0800

    redirect urls for favicon and browser config

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from polls import views
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False
+        ),
+        name='favicon'),
+    path('browserconfig.xml', RedirectView.as_view(
+            url=staticfiles_storage.url('browserconfig.xml'),
+            permanent=False
+        ),
+        name='broswerconfig'),
     path('', views.HomeView.as_view(), name='home'),
     path('poll/<uid>', views.PollView.as_view(), name='poll'),
     path('poll/<uid>/results', views.ResultsView.as_view(), name='results'),
